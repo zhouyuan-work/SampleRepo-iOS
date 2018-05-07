1cb3743badf72624fabc4c06688272024ac80325
g.shu <g.shu@suvt.co.jp>
2018-05-07 16:47:18 +0900

    init project

---
+++ SampleGithubProj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleGithubProj
+//
+//  Created by g.shu on 2018/05/07.
+//  Copyright © 2018 g.shu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
